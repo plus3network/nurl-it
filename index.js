bfc56f65a37aaa9a1b31b8d74ddc1bc87cf0572c
Chris Cowan <me@chriscowan.us>
2012-09-01 16:34:01 -0700

    Adding options and colors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var request = require('request');
 var _       = require('underscore');
+var util    = require('util');
+var colors  = require('colors');
+
 var program    = require('optimist')
-  .usage('Usage: $0')
+  .usage('Usage: $0 [options] <url>')
   .options('h', {
     alias: 'help',
     describe: 'Show this help message'
@@ -14,35 +17,86 @@ var program    = require('optimist')
     alias: 'method',
     default: 'GET',
     describe: 'The request method'
+  })
+  .options('b',{
+    alias: 'body',
+    describe: 'The request body'
+  }).options('n', {
+    alias: 'no-colors',
+    describe: 'Turn off colors'
+  }).options('c', {
+    alias: 'colors',
+    default: true
   });
 
-if(program.argv._.length < 1) {
-  console.log(program.help());
+function extractHeaders (headers) {
+  var finalHeaders = {};
+
+  if (!util.isArray(headers)) {
+    headers = [ headers ];
+  }
+
+  headers.forEach(function (item) {
+    var parts = item.split(':');
+    finalHeaders[parts[0].toLowerCase()] = parts[1];
+  });
+  
+  return finalHeaders;
 }
 
-// var options = {
-//   url: 'http://self.plus3.ws:3000/api/v2/auth/authorize',
-//   method: 'POST',
-//   body: JSON.stringify({ "client_id":"5b7181c3-783b-4a96-9d2c-c16496171592","login":"me@chriscowan.us","password":"monkey" }),
-//   headers: {
-//     "content-type": 'application/json'
-//   }
-// };
-// 
-// var req = request(options, function (err, res, body) {
-//   console.log("\nRequest:\n");
-//   console.log(res.request.method+' '+res.request.path+' HTTP/1.1');
-//   console.log('host: '+res.request.host);
-//   _.each(res.request.headers, function (val,key) {
-//     if(val) {
-//       console.log(key.toLowerCase()+': '+val);
-//     }
-//   });
-//   console.log("\n"+res.request.body);
-//   console.log("\n\nResponse:\n");
-//   console.log('HTTP/'+res.httpVersionMajor+'.'+res.httpVersionMinor+' '+res.statusCode+' '+res.request.httpModule.STATUS_CODES[res.statusCode]);
-//   _.each(res.headers, function (val, key) { 
-//     console.log(key+': '+val);
-//   });
-//   console.log("\n"+body);
-// });
+var useColor = true;
+if (program.argv.n || !program.argv.colors) {
+  colors.mode = 'none';
+  useColor = false;
+}
+
+if(program.argv._.length < 1 || program.argv.h) {
+  console.error(program.help());
+  process.exit();
+}
+
+var options = { url: program.argv._[0], method: program.argv.method };
+
+if (program.argv.body) {
+  options.body = program.argv.body;
+}
+
+if (program.argv.header) {
+  options.headers = extractHeaders(program.argv.header);
+}
+
+
+request(options, function (err, res, body) {
+  if(err) {
+    return console.error(err.stack);
+  }
+
+  console.log("\nRequest:\n");
+  var reqMethodStr = res.request.method+' '+res.request.path+' HTTP/1.1';
+  // Workaround for weird bug with colors module.
+  console.log((useColor)? reqMethodStr.white : reqMethodStr);
+  console.log('host'.cyan+': '+res.request.host.magenta);
+  _.each(res.request.headers, function (val,key) {
+    if (val) {
+      console.log(key.toLowerCase().cyan+': '+(val+'').magenta);
+    }
+  });
+
+  if (res.request.body) {
+    console.log("\n"+(res.request.body+'').yellow);
+  }
+  
+  console.log("\nResponse:\n");
+  var resMethodString = 'HTTP/'+res.httpVersionMajor+'.'+res.httpVersionMinor+' '+res.statusCode+' '+res.request.httpModule.STATUS_CODES[res.statusCode];
+  // Workaround for weird bug with colors module
+  console.log((useColor)? resMethodString.white : resMethodString); 
+  _.each(res.headers, function (val, key) { 
+    if (val) {
+      console.log(key.cyan+': '+(val+'').magenta);
+    }
+  });
+
+  if (body) { 
+    console.log("\n"+body.yellow);
+  }
+});
